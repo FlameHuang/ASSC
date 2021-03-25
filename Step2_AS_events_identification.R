d48225330e8b65bf2ef51c7f00cb92aae0497abe
FlameHuang <923622887@qq.com>
2021-03-25 16:13:13 +0800

    Create Step2_AS_events_identification.R

---
+++ Step2_AS_events_identification.R
@@ -0,0 +1,123 @@
+#### AS type of all AS sites ----------------
+
+GTFIndex <- function(gtfFile, NT = 1){
+  if(!file.exists(gtfFile)) {
+    stop("The GTF file does not exist! And you should provide the GTF file you used in STAR alignment.")
+  }
+
+  # required package ------------------------
+  usePackage("data.table")
+  usePackage("parallel")
+  useBiocPackage('GenomicFeatures')
+
+  # AF/LE -----------------------------------
+  txdb <- GenomicFeatures::makeTxDbFromGFF(file = gtfFile, format = "gtf")
+  txintron <- intronsByTranscript(txdb, use.names=TRUE)
+  txintron <- txintron[S4Vectors::elementNROWS(txintron) > 0]
+  s_s <- as.character(unlist(unique(strand(txintron))))
+  s_l <- S4Vectors::elementNROWS(txintron)
+  txintron_gr <- unlist(txintron)
+  my.rank <- function(s, l) ifelse(s == "+", return(1:l), return(-(l:1)))
+  GenomicRanges::mcols(txintron_gr)$intron_rank <- do.call(c, lapply(seq_along(s_s), function(i) my.rank(s_s[i], s_l[i])))
+  GenomicRanges::mcols(txintron_gr)$tx_id <- names(txintron_gr)
+  txintron_tab_rank <- as.data.table(txintron_gr)
+
+  first_intron_tab <- txintron_tab_rank[abs(intron_rank) == 1, ]
+  first_intron_tab[, junc:=paste0(seqnames, ":", start, "-", end, ":", strand)]
+  setkey(first_intron_tab, tx_id)
+
+  last_intron_tab <- txintron_tab_rank[ , .SD[abs(intron_rank) == max(abs(intron_rank)), ], by = "tx_id"]
+  last_intron_tab[, junc:=paste0(seqnames, ":", start, "-", end, ":", strand)]
+  setkey(last_intron_tab, tx_id)
+
+  # ASS -------------------------------------
+  exons <- as.data.table(unique(exons(txdb, columns = c("gene_id", "EXONNAME"))))
+
+  return(list(first_intron_tab = first_intron_tab, last_intron_tab = last_intron_tab, exons = exons))
+}
+
+
+
+AStype <- function(cell.psi, indexedGTF, NT = 1){
+  # required package ------------------------
+  usePackage("data.table")
+  usePackage("parallel")
+
+  junc_as2 <- as.list(cell.psi[event == 2, id])
+  junc_as <- do.call(rbind, lapply(junc_as2, function(x) {
+    unlist(strsplit(stringr::str_replace_all(x, "-(?=[[:digit:]])", "@"), "[:@]"))
+  }))
+  junc_as <- as.data.table(junc_as)
+  colnames(junc_as) <- paste0(c("chr", "start", "end", "strand"), rep(1:2, c(4,4)))
+  junc_as[, names:=cell.psi[event == 2, as.character(id)]]
+
+  same_start_junc2 <- junc_as[start1 == start2, ]
+  same_end_junc2 <- junc_as[end1 == end2, ]
+  same_start_junc2 <- same_start_junc2[, .(names, chr1, start1, end1, end2, strand1)]
+  colnames(same_start_junc2) <- c("names", "chr", "start", "end1", "end2", "strand")
+  same_end_junc2 <- same_end_junc2[, .(names, chr1, start1, start2, end1, strand1)]
+  colnames(same_end_junc2) <- c("names", "chr", "start1", "start2", "end", "strand")
+
+  match_nage <- same_start_junc2[strand=="-", same_end_junc2[strand=="-",], by = c("names", "chr", "start", "end1", "end2", "strand")]
+  match_posi <- same_start_junc2[strand=="+", same_end_junc2[strand=="+",], by = c("names", "chr", "start", "end1", "end2", "strand")]
+  start_end_match <- rbind(match_posi, match_nage)
+  start_end_match <- start_end_match[as.logical(start_end_match[,2]==start_end_match[,8]), ]
+
+  # SE --------------------------------------
+  SE <- start_end_match[start==start2 & end==end2 & end1 < start1,]
+  SE_junc <- unique(c(SE[[1]], SE[[7]]))
+
+  # MXE -------------------------------------
+  MXE <- start_end_match[start < end1 & end1 < start2 & start2 < end2 & end2 < start1 & start1 < end, ]
+  MXE_junc <- unique(c(MXE[[1]], MXE[[7]]))
+
+  # AF/LE -----------------------------------
+  first_intron_tab <- indexedGTF$first_intron_tab
+  last_intron_tab <- indexedGTF$last_intron_tab
+  junc_as_a <- as.list(cell.psi[, as.character(id)])
+  afle_index <- mcmapply(junc_as_a, FUN = function(x){
+    if(sum(unlist(strsplit(x, "@")) %in% first_intron_tab[, junc]) > 0 ){
+      res = "AFE"
+    } else if (sum(unlist(strsplit(x, "@")) %in% last_intron_tab[, junc]) > 0 ) {
+      res = "ALE"
+    } else {
+      res = "Non"
+    }
+    return(res)
+  }, mc.cores = NT)
+  AFE_junc <- cell.psi[, as.character(id)][which(afle_index == "AFE")]
+  ALE_junc <- cell.psi[, as.character(id)][which(afle_index == "ALE")]
+
+
+  # A3/5SS ----------------------------------
+  same_start_junc2_gr <- GRanges(seqnames = same_start_junc2[, chr],
+                                 ranges = IRanges(start = as.integer(same_start_junc2[, end1]) + 1, end = as.integer(same_start_junc2[, end2])),
+                                 strand = same_start_junc2[, strand],
+                                 names = same_start_junc2[, names])
+  same_end_junc2_gr <- GRanges(seqnames = same_end_junc2[, chr],
+                               ranges = IRanges(start = as.integer(same_end_junc2[, start2]), end = as.integer(same_end_junc2[, start1]) - 1),
+                               strand = same_end_junc2[, strand],
+                               names = same_end_junc2[, names])
+  tmp1 <- as.data.table(same_start_junc2_gr)
+  tmp2 <- indexedGTF$exons
+  tmp3 <- as.data.table(same_end_junc2_gr)
+
+  same_start_ass <- merge(tmp1, tmp2, by = c("seqnames", "start", "strand"))[end.x < end.y, ]
+  same_end_ass <- merge(tmp3, tmp2, by = c("seqnames", "end", "strand"))[start.x > start.y, ]
+  A3SS_junc <- unique(c(same_start_ass[strand == "+", names], same_end_ass[strand == "-", names]))
+  A5SS_junc <- unique(c(same_start_ass[strand == "-", names], same_end_ass[strand == "+", names]))
+
+  AS_list <- list(SE = SE_junc, A3SS = A3SS_junc, A5SS = A5SS_junc, AFE = AFE_junc, ALE = ALE_junc, MXE = MXE_junc)
+
+  # Add AS type ------------------------
+  as.type <-  function(x) {
+    if(sum(mapply(function(y) is.element(x, y), AS_list)) > 0){
+      AS = paste0(names(AS_list)[mapply(function(y) is.element(x, y), AS_list)], collapse = "|")
+    } else {
+      AS = "Other"
+    }
+    return(AS)
+  }
+  cell.psi[!is.na(event), AS:=mcmapply(as.type, cell.psi[!is.na(event), id], mc.cores = NT)]
+  return(cell.psi)
+}
